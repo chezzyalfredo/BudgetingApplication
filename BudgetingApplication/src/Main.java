bc49c8eb1c80f8263600b27d96383823e438b33b
Alfred Wong <chezzyalfredo@gmail.com>
2013-11-03 08:47:05 -0800

    [DEV-0001]  Initial Commit - Main and Statistics
    UI: Console
    Functionality: None
    Date: 11.03.2013
    Version: 0.0.1

---
+++ BudgetingApplication/src/Main.java
@@ -0,0 +1,71 @@
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		int choice = 0;
+		
+		/*Income income = new Income();
+		Expenses expenses = new Expenses();
+		Miscellaneous miscellaneous = new Miscellaneous();
+		*/Statistics stats = new Statistics();
+		
+		do{
+			System.out.print(
+					"== PROGRAM STARTED ==\n" +
+					"1..............Income\n" + 
+					"2............Expenses\n" + 
+					"3.......Miscellaneous\n" +
+					"4....Print Statistics\n" +
+					"0................Exit\n" + 
+					"Please input a value: ");
+			Scanner scan = new Scanner(System.in);
+			try{
+				choice = scan.nextInt();
+			}
+			catch(InputMismatchException x)
+			{
+				choice = -1;
+			}
+			
+			switch (choice) {
+	            case 1:
+	            	//TODO
+	            	System.out.println("input income");
+	                //input = System.in.toString().split(".");
+	            	break;
+	            case 2:  
+	            	//TODO
+	            	System.out.println("input expenses");
+	            	break;
+	            case 3:  
+	            	//TODO
+	            	System.out.println("input miscellaneous");
+	            	break;
+	            case 4:
+	            	//TODO
+	            	/*stats.calculate();
+	            	stats.printStats();*/
+	            	System.out.println("print out statistics here");
+	            	break;
+	            case 0:  
+	        		System.out.println("==TERMINATED==");
+	            	break;
+	            default: 
+	            	System.out.println("[ERROR] " + choice + " is not a valid choice.\n" + 
+	            					   "Please input a correct value.");
+	            	break;
+	        }
+			
+		}while(!(choice == 0));
+		
+	}
+	
+}
---
+++ BudgetingApplication/src/Statistics.java
@@ -0,0 +1,42 @@
+import java.text.DecimalFormat;
+
+
+public class Statistics {
+	double income;
+	double expenses;
+	double misc;
+	DecimalFormat currency;
+	
+	public Statistics()
+	{
+		income = 0.0;
+		expenses = 0.0;
+		misc = 0.0;
+		currency = new DecimalFormat("##.##");
+	}
+	
+	public void inputIncome(double i)
+	{
+		income = i;
+	}
+	
+	public void inputExpenses(double e)
+	{
+		expenses = e;
+	}
+	
+	public void inputMiscellaneous(double m)
+	{
+		misc = m;
+	}
+	
+	public void calculate()
+	{
+		
+	}
+	
+	public void printStats()
+	{
+		
+	}
+}
